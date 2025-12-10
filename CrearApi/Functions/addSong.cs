525935768eeb9381f56006020760b4cfcb998c4c
DrChungo <litingo1000@gmail.com>
2025-12-10 19:19:48 +0100

    Create addSong.cs

---
+++ CrearApi/Functions/addSong.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CrearApi.Functions
+{
+    public static class FunctionsAdd
+    {
+        public static void AddSong()
+        {
+            Console.Clear();
+            Console.WriteLine("=== AÑADIR CANCIÓN ===\n");
+            
+            Console.WriteLine("Esta función aún no está implementada.");
+            Console.WriteLine("Más adelante añadiremos el POST a la API Node.");
+        }
+    }
+}
